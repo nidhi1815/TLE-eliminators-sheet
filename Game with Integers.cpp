0073e4e7bddf8d3e15f57d5aa86b305a5181dd5b
Nidhi Kumari <123724614+nidhi1815@users.noreply.github.com>
2024-03-02 23:03:08 +0530

    Create Game with Integers.cpp

---
+++ Game with Integers.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+signed main()
+{
+    int t;
+    cin >> t;
+    while( t-- )
+    {
+        int n;
+        cin >> n;
+
+        if( n % 3 == 0) cout <<"Second"<<endl;
+        else cout <<"First"<<endl;
+    
+    }
+}
